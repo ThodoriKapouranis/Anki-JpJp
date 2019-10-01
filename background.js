cf44c0e1d54e2771f08327f41cc24395bdf06032
Thodori Kapouranis <51767101+ThodoriKapouranis@users.noreply.github.com>
2019-09-30 21:44:43 -0400

    Added Fetch
    +Grab Definition
    +Grab Pitchaccent (most times)

--- background.js
+++ background.js
@@ -3,14 +3,50 @@ var contextMenuItem = {
     "title": "Send to Anki",
     "contexts": ["selection"]
 }
-var bkr=chrome.extension.getBackgroundPage()
-xmlhttp.onreadystatechange = function(){
-
-}
 chrome.contextMenus.create(contextMenuItem);
+
 
 chrome.contextMenus.onClicked.addListener(function(targetWord){
     if (targetWord.menuItemId =="sendToAnki"){
-        alert(targetWord.selectionText)
+        console.log("Highlighted Word: "+targetWord.selectionText)
+//no-cors cause weblio doesn't allow basic/cor
+        fetch("https://www.weblio.jp/content/+"+targetWord.selectionText, {mode: "cors"})
+            .then(
+                function(response){
+                   // console.log(response.status);
+                   // console.log(response.statusText);
+                   // console.log(response.type);
+                   // console.log(response.url);
+                    //200 response means GOOD, strange
+                    if (response.status !== 200){
+                        console.log("Error code: "+ response.status);
+                        return;
+                    }
+                    
+                    return response.text();
+                }
+            )
+
+            // note that the return of last .then() is the parameter here
+            .then(function(html){
+                var parser = new DOMParser();
+                var doc = parser.parseFromString(html,"text/html");
+                var def = doc.getElementsByClassName("NetDicBody")[0]
+              /*  var defContainer = def.childNodes[1]
+                console.log("Type: "+defContainer.childNodes[0].wholeText + defContainer.childNodes[1].innerText)
+                defContainer.removeChild(defContainer.childNodes[0]);
+                defContainer.removeChild(defContainer.childNodes[0]);*/
+                console.log("Definition: "+def.innerText)
+                console.log("Pitch Accent: "+doc.getElementsByClassName('midashigo')[0].childNodes[4].innerText)
+            }
+
+            )
+            .catch(function(err){
+                console.log('FETCH ERROR:', err);
+            })
+
+
+            
     }
 })
+
